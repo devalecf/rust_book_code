55b1ace3bbd9ebf9531d78a77c9100095fa95c19
devalecf <afessler1998@gmail.com>
2022-05-27 09:57:33 -0500

    hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-a73972001433c395.d
@@ -0,0 +1,5 @@
+C:\Users\15152\Desktop\rust_book\hello_cargo\target\debug\deps\hello_cargo-a73972001433c395.rmeta: src\main.rs
+
+C:\Users\15152\Desktop\rust_book\hello_cargo\target\debug\deps\hello_cargo-a73972001433c395.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\15152\Desktop\rust_book\hello_cargo\target\debug\hello_cargo.exe: C:\Users\15152\Desktop\rust_book\hello_cargo\src\main.rs
